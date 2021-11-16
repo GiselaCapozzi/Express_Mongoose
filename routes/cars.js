902bd84d3165521634ea94259f053ed36f406f14
GiselaCapozzi <gisecapozzi@gmail.com>
2021-11-16 11:55:36 -0300

    Creando Schema

--- routes/cars.js
+++ routes/cars.js
@@ -3,6 +3,38 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
+const carSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+    uppercase: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 99,
+    enum: ['BMW', 'AUDI', 'SEAT']
+  },
+  model: String,
+  sold: Boolean,
+  price: {
+    type: Number,
+    required: function() {
+      return this.sold
+    }
+  },
+  year: {
+    type: Number,
+    min: 2000,
+    max: 2030
+  },
+  extras: [String],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Car = mongoose.model('car', carShema)
+
 // GET
 router
   .get('/list', (req, res) => {
